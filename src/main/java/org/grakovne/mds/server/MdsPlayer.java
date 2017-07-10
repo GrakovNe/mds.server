34a559a363f6af38eebef936e8c22206d61d47c7
GrakovNe <grakovne@yandex.ru>
2017-07-11 02:06:29 +0600

    add plugins

--- src/main/java/org/grakovne/mds/server/MdsPlayer.java
+++ src/main/java/org/grakovne/mds/server/MdsPlayer.java
@@ -3,10 +3,19 @@ package org.grakovne.mds.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application's entry point.
+ */
 @SpringBootApplication
 public class MdsPlayer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MdsPlayer.class, args);
-	}
+    /**
+     * Main application class.
+     *
+     * @param args CLI args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MdsPlayer.class, args);
+    }
 }
+
