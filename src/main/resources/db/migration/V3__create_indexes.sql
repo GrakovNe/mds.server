a10f1e246c4921bd591d467e2050ba1f57b1b842
GrakovNe <grakovne@yandex.ru>
2017-08-03 23:59:30 +0600

    add index migration

---
+++ src/main/resources/db/migration/V3__create_indexes.sql
@@ -0,0 +1,6 @@
+CREATE INDEX author_name ON author (name);
+CREATE INDEX genre_value ON genre (value);
+CREATE INDEX rating_value ON rating (value);
+CREATE INDEX story_title ON story (title);
+CREATE INDEX tag_value ON tag (value);
+CREATE INDEX users_username ON users (username);
