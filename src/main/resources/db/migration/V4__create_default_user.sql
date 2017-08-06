b134c428c2881e467ba4467d3b0c19b25cde3a58
GrakovNe <grakovne@yandex.ru>
2017-08-06 21:52:31 +0600

    change password of default user

--- src/main/resources/db/migration/V4__create_default_user.sql
+++ src/main/resources/db/migration/V4__create_default_user.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (is_account_non_expired, is_account_non_locked, is_credentials_non_expired, is_enabled, password, username)
-VALUES (true, true, true, true, 'e0469addd8d57a3623494096dabc19bebca1a038c9da696940b3f853d106a6ecfa5bd60ce8e72884efa3bd92b930da178fd616f40facad654212d7c2f8817dd4', 'grakovne');
+VALUES (true, true, true, true, '245ceb0e210a6944b659e3f1669e0102e73d1e1ee8b5137cf982fc0be2011e74a66d024bbfbe8a77b33e74ee754457a9961ed4db528a15c3feee4ec16a46fccc', 'grakovne');
 
 insert into users_roles VALUES(1, 1);
 insert into users_roles VALUES(1, 2);
